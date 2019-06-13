773d7b1c4ba5cffe336a7580b617fe03e13f4ee1
Željko Filipin <zeljko.filipin@gmail.com>
2019-06-13 15:13:41 +0200

    change long Gerrit links to short

--- lib/wikitextify.rb
+++ lib/wikitextify.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+def gerrit(url)
+  patch = url.split('/').last
+  "[[gerrit:#{patch}]]"
+end
+
 def phabricator(url)
   task = url.split('/').last
   "[[phabricator:#{task}]]"
--- spec/wikitextify_spec.rb
+++ spec/wikitextify_spec.rb
@@ -3,6 +3,11 @@
 require 'wikitextify'
 
 RSpec.describe 'wikitextify' do
+  it 'changes long Gerrit links to short' do
+    long = 'https://gerrit.wikimedia.org/r/c/mediawiki/extensions/Wikibase/+/507910'
+    short = '[[gerrit:507910]]'
+    expect(gerrit(long)).to eq short
+  end
   it 'changes long Phabricator links to short' do
     long = 'https://phabricator.wikimedia.org/T224324'
     short = '[[phabricator:T224324]]'
