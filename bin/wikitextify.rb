6ecd37b63e7d230b4e1b69ba1fb26896292a9a9f
Željko Filipin <zeljko.filipin@gmail.com>
2019-09-19 16:06:32 +0200

    Replace long Gerrit links with short links
    Example:
    https://gerrit.wikimedia.org/r/394430 > [[gerrit:394430]]

--- bin/wikitextify.rb
+++ bin/wikitextify.rb
@@ -1,3 +1,5 @@
 File.foreach('txt/sos.txt') do |line|
-  puts line.gsub(/(https:\/\/phabricator\.wikimedia\.org\/)*(T\d{6})(#\d{7})*/, '[[phab:\2\3]]')
+  phabricator_line = line.gsub(/(https:\/\/phabricator\.wikimedia\.org\/)*(T\d{6})(#\d{7})*/, '[[phab:\2\3]]')
+  gerrit_line = phabricator_line.gsub(/https:\/\/gerrit\.wikimedia\.org\/r\/(\d{6})/, '[[gerrit:\1]]')
+  puts gerrit_line
 end
