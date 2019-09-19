140b2904991a5af17568765c049a7af7db739420
Željko Filipin <zeljko.filipin@gmail.com>
2019-09-19 15:56:32 +0200

    Create links from Phabricator tasks
    Example: T211443 > [[phab:T211443]]

--- bin/wikitextify.rb
+++ bin/wikitextify.rb
@@ -1,3 +1,3 @@
 File.foreach('txt/sos.txt') do |line|
-  puts line.gsub(/https:\/\/phabricator\.wikimedia\.org\/(T\d{6})(#\d{7})*/, '[[phab:\1\2]]')
+  puts line.gsub(/(https:\/\/phabricator\.wikimedia\.org\/)*(T\d{6})(#\d{7})*/, '[[phab:\2\3]]')
 end
