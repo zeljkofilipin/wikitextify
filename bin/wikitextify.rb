b127631b1dab9976a17434f00c0729e28b9934d8
Željko Filipin <zeljko.filipin@gmail.com>
2019-09-19 15:47:49 +0200

    Replace long Phabricator links (with anchors) with short links
    Example:
    https://phabricator.wikimedia.org/T232691#5488675 > [[phab:T232691#5488675]]

--- bin/wikitextify.rb
+++ bin/wikitextify.rb
@@ -1,3 +1,3 @@
 File.foreach('txt/sos.txt') do |line|
-  puts line.sub(/https:\/\/phabricator\.wikimedia\.org\/(T\d{6})/, '[[phab:\1]]')
+  puts line.sub(/https:\/\/phabricator\.wikimedia\.org\/(T\d{6})(#\d{7})*/, '[[phab:\1\2]]')
 end
