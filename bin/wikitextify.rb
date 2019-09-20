9de11e46cb655c71b9f01d12ecfade47c9adb448
Željko Filipin <zeljko.filipin@gmail.com>
2019-09-20 13:51:27 +0200

    Split long lines into shorter ones

--- bin/wikitextify.rb
+++ bin/wikitextify.rb
@@ -1,5 +1,11 @@
 File.foreach('input.txt') do |line|
-  phabricator_line = line.gsub(/(https:\/\/phabricator\.wikimedia\.org\/)*(T\d{6})(#\d{7})*/, '[[phab:\2\3]]')
-  gerrit_line = phabricator_line.gsub(/https:\/\/gerrit\.wikimedia\.org\/r\/(\d{6})/, '[[gerrit:\1]]')
+  phabricator_line =
+    line.gsub(
+      /(https:\/\/phabricator\.wikimedia\.org\/)*(T\d{6})(#\d{7})*/,
+      '[[phab:\2\3]]')
+  gerrit_line =
+    phabricator_line.gsub(
+      /https:\/\/gerrit\.wikimedia\.org\/r\/(\d{6})/,
+      '[[gerrit:\1]]')
   puts gerrit_line
 end
