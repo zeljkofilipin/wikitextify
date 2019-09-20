656d8dd70d59621aa0e9fa7e28bf58ac99bd805d
Željko Filipin <zeljko.filipin@gmail.com>
2019-09-20 13:56:27 +0200

    Fix RuboCop offenses

--- bin/wikitextify.rb
+++ bin/wikitextify.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 File.foreach('input.txt') do |line|
   phabricator_line =
     line.gsub(
-      /(https:\/\/phabricator\.wikimedia\.org\/)*(T\d{6})(#\d{7})*/,
-      '[[phab:\2\3]]')
+      %r{(https://phabricator\.wikimedia\.org/)*(T\d{6})(#\d{7})*},
+      '[[phab:\2\3]]'
+    )
   gerrit_line =
     phabricator_line.gsub(
-      /https:\/\/gerrit\.wikimedia\.org\/r\/(\d{6})/,
-      '[[gerrit:\1]]')
+      %r{https://gerrit\.wikimedia\.org/r/(\d{6})},
+      '[[gerrit:\1]]'
+    )
   puts gerrit_line
 end
