c3576a22108d7cbcebdefac5a882015a8dc4140c
Željko Filipin <zeljko.filipin@gmail.com>
2019-09-25 12:02:03 +0200

    Inline calling methods on the line

--- bin/wikitextify.rb
+++ bin/wikitextify.rb
@@ -3,10 +3,5 @@
 require('./lib/wikitextify.rb')
 
 File.foreach('input.txt') do |line|
-  phabricator_line = phabricator(line)
-  gerrit_line = gerrit(phabricator_line)
-  git_line = git(gerrit_line)
-  mediawiki_line = mediawiki(git_line)
-  wikitech_line = wikitech(mediawiki_line)
-  puts wikitech_line
+  puts git(gerrit(mediawiki(phabricator(wikitech(line)))))
 end
